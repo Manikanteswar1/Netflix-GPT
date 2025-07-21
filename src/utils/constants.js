f74f9789660b67de8db18df2d9b53f236aedddc9
Manikanteswar1 <baddimanikanta15@gmail.com>
2025-07-21 14:12:17 +0530

    final code-2

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -10,8 +10,6 @@ export const API_OPTIONS = {
     Authorization: 'Bearer '+TMDB_KEY,
   }
 };
-console.log("hi");
-console.log(TMDB_KEY);
 
 export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500/";
 
